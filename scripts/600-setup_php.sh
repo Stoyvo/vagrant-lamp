d7efa49fb19f5ab7976788fbf2fad61e78e566ff
Timothy Stoyanovski <tstoyanovski@demacmedia.com>
2019-01-23 11:52:50 -0500

    Fix opcache config

--- scripts/600-setup_php.sh
+++ scripts/600-setup_php.sh
@@ -23,12 +23,13 @@ function setup_opcache() {
     cat <<EOL >> /opt/phpfarm/inst/php-$1/etc/php.ini
 
 [opcache]
+zend_extension=opcache.so
 opcache.enable=1
 opcache.enable_cli=1
 opcache.interned_strings_buffer=8
 opcache.max_accelerated_files=4000
 opcache.memory_consumption=128
-opcache.revalidate_freq=15
+opcache.revalidate_freq=5
 EOL
 }
 
