5fd80f9300a0ca13e3122ed2e37b1782e520c3bf
Timothy Stoyanovski <tstoyanovski@demacmedia.com>
2019-02-26 11:16:51 -0500

    Fix nvm install

--- scripts/675-setup_nodejs_nvm_yarn.sh
+++ scripts/675-setup_nodejs_nvm_yarn.sh
@@ -20,7 +20,7 @@ echo -e "${YELLOW}* 675-setup_nodejs_nvm_yarn.sh *${NC}"
 echo -e "${YELLOW}********************************${NC}"
 
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
-source ~/.bashrc
+bash
 nvm install 10.15.0
 nvm install 8.2.1
 nvm use 10.15.0
@@ -29,3 +29,4 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update
 apt-get install -y yarn
+exit
